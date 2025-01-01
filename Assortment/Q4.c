90f732b15d20d16216ac90eafcbf533963ad72e8
Dabhi-Pratik <pratikdabhi4902@gmail.com>
2025-01-01 10:45:14 +0530

    Add files via upload

--- Assortment/Q4.c
+++ Assortment/Q4.c
@@ -21,11 +21,68 @@ int main(){
 			printf("a[%d][%d]:",i,j);
 			scanf("%d",&array[i][j]);
 		}
+		
 	}
 	
-	int sum = 0;
+		printf("\n");
 	
+	
+   int sum = 0;
 
+
+	int k;
+	
+	for(i=0 ;i<r ;i++){
+		 printf("Rows :");
+		 
+		for(j=0 ;j<c ;j++){
+			
+			for(k=0 ;k<r ;k++){
+				if(i==k){
+		    	
+		    	printf("%d,",array[i][j]);
+		    	
+	    	    }
+	    	}	
+	    }
+	
+		printf("\n");	
+	  }
+	  
+	  for(i=0; i<r; i++){
+	  	for(j=0; j<c; j++){
+	  		 sum += array[i][j];
+		  }
+		  printf("\n");
+		  printf("The sum of a row :%d",sum);
+		  sum=0;
+		 
+	  }
+	  
+	  printf("\n");
+	   printf("\n");
+		 
+		 
+		for(j=0 ;j<c ;j++){
+			  printf("column :");
+			for(k=0 ;k<r ;k++){
+		    printf("%d,",array[k][j]);   
+	    	}	
+	    	 printf("\n");
+	    }
+   
+	for(j=0; j<c; j++){
+		 for(k=0; k<c; k++){
+		 
+		 
+	  		 sum += array[k][j];
+		  }
+		  printf("\n");
+		  printf("The sum of a column :%d",sum);
+		  sum=0;
+		 
+	  }
+  
 		
 	
 
